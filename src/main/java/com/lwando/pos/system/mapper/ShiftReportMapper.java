5ff4dce0be99882221f621f9337424f4ec79847a
LwandoXabiso <lxb.sigalelana@gmail.com>
2026-01-29 22:06:07 +0200

    Done working on the ShiftReportService Api

---
+++ src/main/java/com/lwando/pos/system/mapper/ShiftReportMapper.java
@@ -0,0 +1,54 @@
+package com.lwando.pos.system.mapper;
+
+import com.lwando.pos.system.model.Order;
+import com.lwando.pos.system.model.Product;
+import com.lwando.pos.system.model.Refund;
+import com.lwando.pos.system.model.ShiftReport;
+import com.lwando.pos.system.payload.dto.OrderDTO;
+import com.lwando.pos.system.payload.dto.ProductDTO;
+import com.lwando.pos.system.payload.dto.RefundDTO;
+import com.lwando.pos.system.payload.dto.ShiftReportDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ShiftReportMapper {
+
+    public static ShiftReportDTO toDTO(ShiftReport shiftReport) {
+        return ShiftReportDTO.builder()
+                .id(shiftReport.getId())
+                .shiftEnd(shiftReport.getShiftEnd())
+                .shiftStart(shiftReport.getShiftStart())
+                .totalSales(shiftReport.getTotalSales())
+                .totalRefunds(shiftReport.getTotalRefunds())
+                .netSale(shiftReport.getNetSale())
+                .totalOrders(shiftReport.getTotalOrders())
+                .cashier(UserMapper.toDTO(shiftReport.getCashier()))
+                .cashierId(shiftReport.getCashier().getId())
+                .branchId(shiftReport.getBranch().getId())
+                .recentOrders(mapOrders(shiftReport.getRecentOrders()))
+                .topSellingProducts(mapProducts(shiftReport.getTopSellingProducts()))
+                .refunds(mapRefunds(shiftReport.getRefunds()))
+                .paymentSummaries(shiftReport.getPaymentSummaries())
+
+                .build();
+    }
+
+    private static List<RefundDTO> mapRefunds(List<Refund> refunds) {
+        if (refunds == null || refunds.isEmpty()) return null;
+
+        return refunds.stream().map(RefundMapper::toDTO).collect(Collectors.toList());
+    }
+
+    private static List<ProductDTO> mapProducts(List<Product> topSellingProducts) {
+        if (topSellingProducts == null || topSellingProducts.isEmpty()) return null;
+
+        return topSellingProducts.stream().map(ProductMapper::toDTO).collect(Collectors.toList());
+    }
+
+    private static List<OrderDTO> mapOrders(List<Order> recentOrders) {
+        if (recentOrders == null || recentOrders.isEmpty()) return null;
+
+        return recentOrders.stream().map(OrderMapper::toDTO).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/lwando/pos/system/payload/dto/ShiftReportDTO.java
@@ -0,0 +1,46 @@
+package com.lwando.pos.system.payload.dto;
+
+import com.lwando.pos.system.model.PaymentSummary;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShiftReportDTO {
+
+    private Long id;
+
+    private LocalDateTime shiftStart;
+
+    private LocalDateTime shiftEnd;
+
+    private Double totalSales;
+
+    private Double totalRefunds;
+
+    private Double netSale;
+
+    private int totalOrders;
+
+    private UserDTO cashier;
+
+    private Long cashierId;
+
+    private Long branchId;
+
+    private BranchDTO branch;
+
+    private List<PaymentSummary> paymentSummaries;
+
+    private List<ProductDTO> topSellingProducts;
+
+    private List<OrderDTO> recentOrders;
+
+    private List<RefundDTO> refunds;
+}
