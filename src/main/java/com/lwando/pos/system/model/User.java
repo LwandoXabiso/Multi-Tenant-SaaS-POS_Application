a96b3c77d3641317834297441c85675dad320396
LwandoXabiso <lxb.sigalelana@gmail.com>
2026-01-29 21:57:01 +0200

    Hand to refactor some code on some of the Apis

---
+++ src/main/java/com/lwando/pos/system/model/User.java
@@ -0,0 +1,98 @@
+package com.lwando.pos.system.model;
+
+import com.lwando.pos.system.domain.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "user_email_unique", columnNames = "email"),
+                @UniqueConstraint(name = "profile_image_id_unique", columnNames = "profileImageId")
+        }
+)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email(message = "Email should be valid")
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    private Store store;
+
+    @ManyToOne
+    private Branch branch;
+
+    private String phoneNumber;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @NotNull
+    @Past
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private UserRole role;
+
+    @NotNull
+    @PastOrPresent
+    @Column(nullable = false)
+    private LocalDateTime registeredAt;
+
+    @PastOrPresent
+    private LocalDateTime updatedAt;
+
+    @PastOrPresent
+    private LocalDateTime lastLoginAt;
+
+    @Column(unique = true)
+    private String profileImageId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @PrePersist
+    protected void onCreate() {
+        registeredAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+    private LocalDateTime createdAt;
+    //private LocalDateTime updatedAt;
+    private LocalDateTime lastLogin;
+
+
+
+}
