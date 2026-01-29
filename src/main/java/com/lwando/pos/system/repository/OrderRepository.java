aa4a6e0cc5f0dd3b3d78354108c40692f1f72316
LwandoXabiso <lxb.sigalelana@gmail.com>
2026-01-29 22:25:01 +0200

    Done working on the OrderRepository Api

---
+++ src/main/java/com/lwando/pos/system/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package com.lwando.pos.system.repository;
+
+import com.lwando.pos.system.model.Order;
+import com.lwando.pos.system.model.User;
+import jakarta.validation.constraints.PastOrPresent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByCustomerId(Long customerId);
+
+    List<Order> findByBranchId(Long branchId);
+
+    List<Order> findByCashierId(Long cashierId);
+
+    List<Order> findByBranchIdAndCreatedAtBetween(Long branchId, LocalDateTime from, LocalDateTime to);
+
+    List<Order> findByCashierAndCreatedAtBetween(User cashier, @PastOrPresent LocalDateTime createdAt, @PastOrPresent LocalDateTime createdAt2);
+
+    List<Order> findTop5ByBranchIdOrderByCreatedAtDesc(Long branchId);
+}
